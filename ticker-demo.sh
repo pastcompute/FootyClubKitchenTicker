6066d300cab6d0e4cd484016bdb73315ccbca541
Andrew McDonnell <bugs@andrewmcdonnell.net>
2014-03-08 00:00:34 +1030

    Convert to markdown, and added OpenWRT build notes

--- ticker-demo.sh
+++ ticker-demo.sh
@@ -3,4 +3,5 @@
 # apt-get install ticker
 # Need to work out the widths for 1920, etc @ given larger font size
 
-xterm -uc -cr black -bg black -font lucidasanstypewriter-bold-24 -geom 33x1 -e ticker --foreground=yellow --background=black -l Test123
+xterm -uc -cr black -bg black -font lucidasanstypewriter-bold-24 -geom 33x1 -bw 0 \
+    -e ticker --foreground=yellow --background=black --delay=0.2 -l Test123
